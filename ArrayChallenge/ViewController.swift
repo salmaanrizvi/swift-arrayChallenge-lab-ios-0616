0f90182a6de38245fba91d763e1d594b96479579
salmaanrizvi <sar228@cornell.edu>
2016-07-10 21:29:35 -0400

    tests passing

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,13 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
-
+    let quantityList : [Int] = [6, 4, 12, 4]
+    let itemList : [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        makeShoppingList(itemList, quantityOfItems: quantityList)
+        print(shoppingList)
+        
+    }
     
+    func makeShoppingList(items : [String], quantityOfItems: [Int]) -> [String] {
+        for (index, item) in items.enumerate() {
+            shoppingList.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+        }
         
-        
+        return shoppingList
     }
     
     
